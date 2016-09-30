f62b520a2851da076876af2f3d33e94ca2ff7b95
Ivan Magda <imagda15@gmail.com>
2016-09-30 13:27:49 +0900

    Implement binary search problem

---
+++ 1.Algoritmic_Toolbox/BinarySearch/BinarySearch.java
@@ -0,0 +1,83 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BinarySearch {
+
+    private static int binarySearch(int[] a, int targetValue) {
+        int min = 0;
+        int max = a.length - 1;
+        int guess;
+
+        while (min <= max) {
+            guess = min + (max - min) / 2;
+            if (targetValue == a[guess]) {
+                return guess;
+            } else if (a[guess] < targetValue) {
+                min = guess + 1;
+            } else {
+                max = guess - 1;
+            }
+        }
+
+        return -1;
+    }
+
+    private static int linearSearch(int[] a, int x) {
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] == x) return i;
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        FastScanner scanner = new FastScanner(System.in);
+
+        int n = scanner.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = scanner.nextInt();
+        }
+
+        int m = scanner.nextInt();
+        int[] b = new int[m];
+        for (int i = 0; i < m; i++) {
+            b[i] = scanner.nextInt();
+        }
+
+        for (int i = 0; i < m; i++) {
+            System.out.print(binarySearch(a, b[i]) + " ");
+        }
+    }
+
+    private static class FastScanner {
+        BufferedReader br;
+        StringTokenizer st;
+
+        FastScanner(InputStream stream) {
+            try {
+                br = new BufferedReader(new InputStreamReader(stream));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+    }
+}
+
